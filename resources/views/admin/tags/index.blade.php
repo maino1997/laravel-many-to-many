bb3def67fe36fab49ed8d3e0ae5717baede4ac8a
Sasha Mainardi <sashamainardi4@gmail.com>
2022-04-07 16:28:41 +0200

    added container to tags.index.php BONUS VERSION

--- resources/views/admin/tags/index.blade.php
+++ resources/views/admin/tags/index.blade.php
@@ -1,21 +1,23 @@
 @extends('layouts.app')
 @section('content')
-    <table class="table table-dark">
-        <thead>
-            <tr>
-                <th scope="col">#</th>
-                <th scope="col">Nome</th>
-                <th scope="col">Colore</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($tags as $tag)
+    <div class="container">
+        <table class="table table-dark">
+            <thead>
                 <tr>
-                    <th scope="row">{{ $tag->id }}</th>
-                    <td>{{ $tag->name }}</td>
-                    <td>{{ $tag->color }}</td>
+                    <th scope="col">#</th>
+                    <th scope="col">Nome</th>
+                    <th scope="col">Colore</th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                @foreach ($tags as $tag)
+                    <tr>
+                        <th scope="row">{{ $tag->id }}</th>
+                        <td>{{ $tag->name }}</td>
+                        <td>{{ $tag->color }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 @endsection
